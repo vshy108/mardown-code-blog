5e1e7704a32961531a8a84b53d6b425bf15ca3a3
V-SHY <vshy108@gmail.com>
2019-03-02 13:33:27 +0800

    🚧 Add code syntax highlighting gem

---
+++ lib/blog_render.rb
@@ -0,0 +1,7 @@
+require 'redcarpet'
+require 'rouge'
+require 'rouge/plugins/redcarpet'
+# Custom renderer for Markdown syntax
+class BlogRender < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+end
