68ef271e7b129ed7c5aef4f014d615c6425af4f7
V-SHY <vshy108@gmail.com>
2019-03-10 15:12:56 +0800

    🗃 Add title to blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_10_024248) do
+ActiveRecord::Schema.define(version: 2019_03_10_070325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_03_10_024248) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.string "tags", default: [], array: true
+    t.string "title"
     t.index ["tags"], name: "index_blogs_on_tags", using: :gin
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
