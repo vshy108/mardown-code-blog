e35721c7b7daa56ce0854894a8be6a519a88d84f
V-SHY <vshy108@gmail.com>
2019-03-10 15:02:55 +0800

    🚧 Use blogs#new as root page
    Use two buttons to save as draft and publish blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,10 +5,32 @@ class BlogsController < ApplicationController
     @blogs = current_user.blogs
   end
 
+  def new
+    @blog = Blog.new
+  end
+
   def create
-    blog = Blog.new(content: blog_params[:content], user: User.first)
+    status = if Blog.statuses.include? params[:status]
+               params[:status]
+             else
+               'draft'
+             end
+    blog = Blog.new(
+      content: blog_params[:content], user: current_user, status: status
+    )
     blog.save
-    redirect_to root_path
+    if status == 'draft'
+      redirect_to edit_blog_path(id: blog.id)
+    else
+      redirect_to new_blog_path
+    end
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :blogs
   devise_for :users
   get 'homes/index'
-  root to: 'homes#index'
+  root to: 'blogs#new'
 end
